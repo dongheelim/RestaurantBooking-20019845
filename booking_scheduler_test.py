b010a9166b34e6952f49b8d242b2de82542f5063
donghee.lim <ldh5730@naver.com>
2024-07-11 11:04:42 +0900

    [unittest] ~step6-2

--- booking_scheduler_test.py
+++ booking_scheduler_test.py
@@ -2,6 +2,7 @@ import unittest
 from datetime import datetime, timedelta
 
 from booking_scheduler import BookingScheduler
+from communication_test import TestableSmsSender
 from schedule import Customer, Schedule
 
 NOT_ON_THE_HOUR = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
@@ -50,8 +51,13 @@ class BookingSchedulerTest(unittest.TestCase):
         self.assertEqual("Number of people is over restaurant capacity per hour", str(context.exception))
 
     def test_예약완료시_SMS는_무조건_발송(self):
-        pass
+        testable_sms_sender = TestableSmsSender()
+        schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+        self.booking_scheduler.set_sms_sender(testable_sms_sender)
 
+        self.booking_scheduler.add_schedule(schedule)
+
+        self.assertTrue(testable_sms_sender.is_send_method_is_called())
     def test_이메일이_없는_경우에는_이메일_미발송(self):
         pass
 
