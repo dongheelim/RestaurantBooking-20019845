7253e59688b41a2df8b3b00c3802502dfc2fe99a
donghee.lim <ldh5730@naver.com>
2024-07-11 11:47:01 +0900

    [unittest] ~step12 TestableScheduler로 대체

--- booking_scheduler_test.py
+++ booking_scheduler_test.py
@@ -15,14 +15,13 @@ UNDER_CAPACITY = 1
 CAPACITY_PER_HOUR = 3
 
 
-class SundayBookingScheduler(BookingScheduler):
-    def get_now(self):
-        return datetime.strptime("2021/03/28 17:00", "%Y/%m/%d %H:%M")
+class TestableScheduler(BookingScheduler):
+    def __init__(self, capacity_per_hour, date_time):
+        super().__init__(capacity_per_hour)
+        self.__date_time = date_time
 
-
-class MondayBookingScheduler(BookingScheduler):
     def get_now(self):
-        return datetime.strptime("2024/06/03 17:00", "%Y/%m/%d %H:%M")
+        return datetime.strptime(self.__date_time, "%Y/%m/%d %H:%M")
 
 
 class BookingSchedulerTest(unittest.TestCase):
@@ -90,16 +89,15 @@ class BookingSchedulerTest(unittest.TestCase):
         self.assertEqual(self.testable_mail_sender.get_count_send_mail_is_called(), 1)
 
     def test_현재날짜가_일요일인_경우_예약불가_예외처리(self):
-        self.booking_scheduler = SundayBookingScheduler(CAPACITY_PER_HOUR)
+        self.booking_scheduler = TestableScheduler(CAPACITY_PER_HOUR, "2024/7/7 17:00")
 
         with self.assertRaises(ValueError):
             new_schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER_WITH_MAIL)
             self.booking_scheduler.add_schedule(new_schedule)
             self.fail()
 
-
     def test_현재날짜가_일요일이_아닌경우_예약가능(self):
-        self.booking_scheduler = MondayBookingScheduler(CAPACITY_PER_HOUR)
+        self.booking_scheduler = TestableScheduler(CAPACITY_PER_HOUR, "2024/7/8 17:00")
 
         new_schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER_WITH_MAIL)
         self.booking_scheduler.add_schedule(new_schedule)
