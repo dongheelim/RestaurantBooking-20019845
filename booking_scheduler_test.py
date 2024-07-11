e35557a2a69fc2ceac2b9cedc0f0a01f03e42d98
donghee.lim <ldh5730@naver.com>
2024-07-11 10:59:11 +0900

    [unittest] ~step4

--- booking_scheduler_test.py
+++ booking_scheduler_test.py
@@ -8,26 +8,36 @@ NOT_ON_THE_HOUR = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
 ON_THE_HOUR = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
 CUSTOMER = Customer("Fake name", "010-1234-5678")
 
+UNDER_CAPACITY = 1
+CAPACITY_PER_HOUR = 3
+
 
 class BookingSchedulerTest(unittest.TestCase):
 
-    def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(self):
-        schedule = Schedule(NOT_ON_THE_HOUR, 1, CUSTOMER)
-        booking_scheduler = BookingScheduler(3)
+    def setUp(self):
+        self.booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
+        super().setUp()
 
+    def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(self):
+        schedule = Schedule(NOT_ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
         with self.assertRaises(ValueError):
-            booking_scheduler.add_schedule(schedule)
+            self.booking_scheduler.add_schedule(schedule)
 
     def test_예약은_정시에만_가능하다_정시인_경우_예약가능(self):
-        schedule = Schedule(ON_THE_HOUR, 1, CUSTOMER)
-        booking_scheduler = BookingScheduler(3)
+        schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+        self.booking_scheduler.add_schedule(schedule)
 
-        booking_scheduler.add_schedule(schedule)
-
-        self.assertTrue(booking_scheduler.has_schedule(schedule))
+        self.assertTrue(self.booking_scheduler.has_schedule(schedule))
 
     def test_시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생(self):
-        pass
+        schedule = Schedule(ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER)
+        self.booking_scheduler.add_schedule(schedule)
+
+        with self.assertRaises(ValueError) as context:
+            new_schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+            self.booking_scheduler.add_schedule(new_schedule)
+
+        self.assertEqual("Number of people is over restaurant capacity per hour", str(context.exception))
 
     def test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공(self):
         pass
